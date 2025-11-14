048843252998a5355cdc03f5355b5d6a40d1b9cb
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-14 21:37:48 +0530

    checking password checker

--- 02_Variable_&_DataType/index.js
+++ 02_Variable_&_DataType/index.js
@@ -1,6 +1,26 @@
 console.log("Manas!")
 //comment 
 //first name
-let firstName = "MD";
-let secondName = "Fazal"
-console.log("FulllName:",firstName + " " + secondName);
+// let firstName = "MD";
+// let secondName = "Fazal"
+// console.log("FulllName:",firstName + " " + secondName);
+
+// let price1=130;
+// let price2= 160;
+// let totalPrice = price1+price2;
+// console.log(totalPrice);
+
+
+// let val = prompt("Enter your value:")
+// console.log(val,typeof val)
+
+
+//Password checker
+let userID = "mdfazalbarh";
+let password = "1234";
+
+let dbuserId = "mdfazalbarh";
+let dbpassword = "1234"
+
+let result = userID === dbuserId &&  password === dbpassword ? "successfuly LoggedIn" :"wrongPassword"
+console.log(result)
