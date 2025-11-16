c075b32addf6fd9fad9ca93c23a846e8ba0a04f1
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-16 19:01:35 +0530

    learning about function

---
+++ 06_Js_Funtion/index.js
@@ -0,0 +1,48 @@
+
+
+// setTimeout(function (){
+//     alert("Please Login")
+// },5000)
+
+
+//5th. Write a function that takes a traffic light color and gives the correct instruction (e.g. "go" 
+// for green, "stop" for red, "caution" for yellow, and "invalid color" for anything else).
+
+// function traficLight(val){
+//     if(val === "go"){
+//         console.log("Green")
+//     }else if(val === "stop"){
+//         console.log("Red")
+//     }else if(val === "caution"){
+//             console.log("Yellow")
+//     }else{
+//         console.log("invalid color")
+//     }
+
+// }
+
+// traficLight("st")
+// function sumOfN(val){
+//     let sum = 0;
+//     for(let i=1;i<=val;i++){
+
+//         sum = sum + i;
+//     }
+//   console.log(sum)
+// }
+// sumOfN(5);
+
+// function fact(val){
+//      let factorial = 1;
+//     for(let i=1;i<=BigInt(val);i++ ){
+       
+//         factorial = factorial*i;
+//     }
+//    return factorial;
+// }
+
+// let facto = fact(5);
+// console.log(facto)
+
+
+
