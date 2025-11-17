2ab1d25be149ee771a5880e22769bca82d721766
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-17 18:37:56 +0530

    Learning about String in js

--- String_vs_Array_vs_Object/index.js
+++ String_vs_Array_vs_Object/index.js
@@ -1,14 +1,26 @@
 //String
-console.log('fazal')
+// console.log('fazal')
 
-let str = "manas"
-let str2 = 'Manas'
+// let str = "manas"
+// let str2 = 'Manas'
+// console.log(str)
+// console.log(str2)
+// let str3 = `fazal`
+// console.log(str3,typeof str3)
+
+// let firstName = "Md"
+// let lastName = "fazal"
+// let fullName = `${firstName} ${lastName}`
+// console.log(`My Name is ${fullName}`)
+
+// let a = 10;
+// let b = 20; 
+// let c = 30;
+// console.log(`a value is = ${a} b value is = ${b} c value is = ${c}`)
+// `` are template literal,${} are called placeholder
+// ${a} are called string interpolation
+
+
+let str = new String("Manas");
 console.log(str)
-console.log(str2)
-let str3 = `fazal`
-console.log(str3,typeof str3)
 
-let firstName = "Md"
-let lastName = "fazal"
-let fullName = `${firstName} ${lastName}`
-console.log(`My Name is ${fullName}`)
