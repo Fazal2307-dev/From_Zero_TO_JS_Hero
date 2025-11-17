4d0c466f3fe4062eaaf952afea5a68e248254ea4
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-17 18:14:27 +0530

    Learning about string

--- String_vs_Array_vs_Object/index.js
+++ String_vs_Array_vs_Object/index.js
@@ -1,2 +1,14 @@
 //String
 console.log('fazal')
+
+let str = "manas"
+let str2 = 'Manas'
+console.log(str)
+console.log(str2)
+let str3 = `fazal`
+console.log(str3,typeof str3)
+
+let firstName = "Md"
+let lastName = "fazal"
+let fullName = `${firstName} ${lastName}`
+console.log(`My Name is ${fullName}`)
