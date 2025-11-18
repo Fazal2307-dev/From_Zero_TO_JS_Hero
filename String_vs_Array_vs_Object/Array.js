d6b9b63b79b7feca32d9558d656da794cfb4b5bc
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-18 18:18:56 +0530

    loop in js

--- String_vs_Array_vs_Object/Array.js
+++ String_vs_Array_vs_Object/Array.js
@@ -1,3 +1,30 @@
 //-------------------------ARRAY----------------------------------//
-let arr = [23,45,67,443,32];
-console.log(arr[3])
+// let arr = [23,45,67,443,32,[34,23,26]];
+// console.log(arr[5[1]])
+
+//constructor
+
+// let arr2 = new Array(2,4,4,"fsadd",("Faszal"))
+// console.log(arr2)
+
+// let arr = [20]
+// let con = new Array(20);
+// console.log(arr,typeof arr)
+// console.log(con,typeof con) object
+
+// let arr = [2,3,4,5,6]
+// console.log(arr)//2,3,4,5,6
+// arr[2]=99;
+// console.log(arr);//2,3,99,5,6
+//that means array are mutable 
+
+let arr = [1,4,7,8,9]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+// for(let elem of arr){
+//     console.log(elem)
+// }
+// for(let index in arr)(
+//     console.log(index)
+// )
