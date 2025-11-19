cfee4ab5577eab16eef4cc17c570d6510aeea765
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-19 19:47:08 +0530

    constructor in js

--- String_vs_Array_vs_Object/Object.js
+++ String_vs_Array_vs_Object/Object.js
@@ -1,8 +1,28 @@
 //--------------------------Object--------------------------
-let user = {
-    name:"FAZAL",
-    email:"mdfazalbarh@gmail.com",
-    Address:"Barh,Patna"
-}
-console.log(user.name)
-console.log(user)
+// let user = {
+//     name:"FAZAL",
+//     email:"mdfazalbarh@gmail.com",
+//     Address:"Barh,Patna"
+// }
+// console.log(user.name)
+// console.log(user)
+
+// let car = {
+//     brand:"toyota",
+//     color:"red",
+//     speed:120,
+//     drive:function(){
+//        return "Car is running"
+//     },
+//     stop:()=>{
+//      return "stopping the car"
+//     }
+// }
+// console.log(car.drive())
+// console.log(car["brand"])
+
+// let obj = new Object({
+//     name:"Fazal"
+// })
+// obj.age = 23;
+// console.log(obj)
