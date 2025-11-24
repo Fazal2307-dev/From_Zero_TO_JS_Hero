2a7bd265039a8b0624c13b270e573667bf77521d
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-24 21:45:34 +0530

    q1

--- 08_String_vs_Array_vs_Object/Object.js
+++ 08_String_vs_Array_vs_Object/Object.js
@@ -84,3 +84,21 @@
 
 // let  {name,age,address}=user;
 // console.log(name,age,address)
+
+//#-----------------------------------Question and answer--------------------------------------------------------------------------------#
+/*Q1. Create a person object with properties: name, age, and city. Then
+a) Log each property as: value of “name” property is “manas” using loop.
+b) Add a new property called email to the person object.
+c) Delete “city” property from person object.*/ 
+let person ={
+    name:"Fazal",
+    age:24,
+    city:"Barh",
+}
+person.email="mdfazalbarh@gmail.com";
+delete person.city
+for(let key in person){
+    console.log(`Value of ${key} is ${person[key]}`)
+}
+
+console.log(person)
